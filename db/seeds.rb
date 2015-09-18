eff16746113a4e2706ed7e114f8afb60c48d6c4d
Chris McCammon <cmccammon@gmail.com>
2015-09-18 16:01:19 -0500

    fished seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,40 @@
 
-Table.all.each do |course|
-  5.times do
-    Student.create({
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      gpa: random_gpa,
-      course_id: course.id
-    })
-  end
+
+
+
+
+50.times do
+ user = User.create({
+   first_name: Faker::Name.first_name,
+   last_name: Faker::Name.last_name,
+   age: Faker::Number.between(18, 85)
+   })
+ Address.create({
+   user_id: user.id,
+   street: Faker::Address.street_address,
+   city: Faker::Address.city,
+   state: Faker::Address.state_abbr,
+   zip: Faker::Address.zip
+   })
 end
-create_table :orders do |t|
-      t.string :item_id
-      t.string :user_id
-      t.string :quantity, null: false
 
-create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
-      t.integer :age, default: 0
 
- create_table :addresses do |t|
-      t.string :user_id
-      t.string :street_name
-      t.string :city
-      t.string :state
-      t.string :zip
 
-      create_table :items do |t|
-      t.string :name, null: false
-      t.string :price, default: 0
-      t.string :description
+
+50.times do
+  items = Item.create({
+    name:  Faker::
+    price: Faker::Number.between(10, 210)
+    description: Faker::
+    })
+end
+
+
+
+25.times do
+  orders = Orders.create({
+    item_id: Faker::Number.between(1, 50)
+    user_id: Faker::Number.between(1, 50)
+    quantity: Faker::Number.between(1, 10)
+    })
+end
