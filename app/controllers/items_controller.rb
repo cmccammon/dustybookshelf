680e1d9b28ca28ff92f141c9ec5ccb9222d4c814
Chris McCammon <cmccammon@gmail.com>
2015-09-20 12:26:54 -0500

    controllers about done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,49 +5,47 @@ class ItemsController < ApplicationController
   end
 
   def show
-    if Address.exists?(params[:id])
-      addresses = Address.find(params[:id])
-      render json: addresses.to_json, status: 200
+    if Item.exists?(params[:id])
+      item = Item.find(params[:id])
+      render json: item.to_json, status: 200
     else
       render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
     end
   end
 
   def create
-    if params[:street_name].nil? || params[:street_name].empty?
+    if params[:name].nil? || params[:name].empty?
       err_msg = "The 'street_name' parameter was empty or not found"
       render json: { error_msg: err_msg }.to_json, status: 422
     else
-      address = Address.new
-      address.street_name = params[:street_name]
-      address.city = params[:city]
-      address.state = params[:state]
-      address.zip = params[:zip]
-      address.save
-      render json: address.to_json, status: 201
+      item = Item.new
+      item.name = params[:name]
+      item.price = params[:price]
+      item.description = params[:description]
+      item.save
+      render json: item.to_json, status: 201
     end
 
   end
 
   def update
-    if address.exists?(params[:id])
-      address = Address.find(params[:id])
-      address.street_name = params[:street_name]
-      address.city = params[:city]
-      address.state = params[:state]
-      address.zip = params[:zip]
-      address.save
-      render json: address.to_json, status: 200
+    if item.exists?(params[:id])
+      item = item.find(params[:id])
+      item.name = params[:name]
+      item.price = params[:price]
+      item.description = params[:description]
+      item.save
+      render json: item.to_json, status: 200
     else
       render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
     end
   end
 
   def destroy
-    if Address.exists?(params[:id])
-      address = Address.find(params[:id])
-      address.destroy
-      render json: { message: "Address record Deleted Successfully." }, status: 200
+    if item.exists?(params[:id])
+      item = item.find(params[:id])
+      item.destroy
+      render json: { message: "Item record Deleted Successfully." }, status: 200
     else
       render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
     end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,49 +5,48 @@ class OrdersController < ApplicationController
   end
 
   def show
-    if Address.exists?(params[:id])
-      addresses = Address.find(params[:id])
-      render json: addresses.to_json, status: 200
+    if Order.exists?(params[:id])
+      orderes = Order.find(params[:id])
+      render json: orderes.to_json, status: 200
     else
       render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
     end
   end
 
   def create
-    if params[:street_name].nil? || params[:street_name].empty?
-      err_msg = "The 'street_name' parameter was empty or not found"
+    if params[:item_id].nil? && params[:user_id].nil? || params[:item_id].empty? && params[:user_id].empty?
+      err_msg = "The 'item_id' or 'user_id' parameter was empty or not found"
       render json: { error_msg: err_msg }.to_json, status: 422
     else
-      address = Address.new
-      address.street_name = params[:street_name]
-      address.city = params[:city]
-      address.state = params[:state]
-      address.zip = params[:zip]
-      address.save
-      render json: address.to_json, status: 201
+      order = Order.new
+      order.item_id = params[:item_id]
+      order.order_id = params[:order_id]
+      order.quantity = params[:quantity]
+      order.save
+      render json: order.to_json, status: 201
     end
 
   end
 
   def update
-    if address.exists?(params[:id])
-      address = Address.find(params[:id])
-      address.street_name = params[:street_name]
-      address.city = params[:city]
-      address.state = params[:state]
-      address.zip = params[:zip]
-      address.save
-      render json: address.to_json, status: 200
+    if Order.exists?(params[:id])
+      order = Order.find(params[:id])
+      order.street_name = params[:street_name]
+      order.city = params[:city]
+      order.state = params[:state]
+      order.zip = params[:zip]
+      order.save
+      render json: order.to_json, status: 200
     else
       render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
     end
   end
 
   def destroy
-    if Address.exists?(params[:id])
-      address = Address.find(params[:id])
-      address.destroy
-      render json: { message: "Address record Deleted Successfully." }, status: 200
+    if Order.exists?(params[:id])
+      order = Order.find(params[:id])
+      order.destroy
+      render json: { message: "order record Deleted Successfully." }, status: 200
     else
       render json: { error_msg: 'Record Not Found!', id: params[:id] }.to_json, status: 404
     end
